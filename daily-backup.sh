99bbb2f343a575ce599efbb4f4b050a2f980dbe0
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-04-19 22:06:44 -0700

    Add files via upload

---
+++ daily-backup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SCRIPTDIR=/home/crosswalkcalvin/tankpit-flowers.github.io
+
+DATESTRING=$(date +%Y-%m-%d) # 2017-01-01
+
+cp $SCRIPTDIR/data/flowers_stats_alltime.csv $SCRIPTDIR/data/flowers_stats_$DATESTRING.csv
+cp $SCRIPTDIR/data/flowers_stats_now.csv $SCRIPTDIR/data/flowers_stats_alltime.csv
