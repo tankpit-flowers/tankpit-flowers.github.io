01a54ec709f3e9a37df21e320a5c0e4657431b5e
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-04-25 22:47:32 -0700

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -357,7 +357,7 @@ def make_roster_md(roster_out_file, df_now, flower_dict = flower_dict, last_upda
     roster.write('\n## ROSTER\n\n')
     roster.write('{:.roster}\n')
     roster.write('|<span class="roster_header">Flower</span>')
-    roster.write('|<span class="roster_header">Main Tank</span>|')
+    roster.write('|<span class="roster_header">Main Tank</span>|\n')
     # main flowers
     for j in range(len(flower_dict)):
         for i in flower_dict.values():
@@ -380,7 +380,7 @@ def make_roster_md(roster_out_file, df_now, flower_dict = flower_dict, last_upda
     # alts
     roster.write('{:.roster}\n')
     roster.write('|<span class="roster_header">Alt</span>')
-    roster.write('|<span class="roster_header">Main Flower</span>|')
+    roster.write('|<span class="roster_header">Main Flower</span>|\n')
     for j in range(len(flower_dict)):
         for i in flower_dict.values():
             if i['seq'] == j and i['section'] == 1:
