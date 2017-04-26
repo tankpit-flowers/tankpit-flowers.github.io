1a9c671dc82e052392defd45d7cce359836ae7f7
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-04-25 22:39:54 -0700

    Add files via upload

--- daily-backup.sh
+++ daily-backup.sh
@@ -11,3 +11,4 @@ cp $SCRIPTDIR/data/flowers_hours_log.csv $SCRIPTDIR/data/flowers_hours_log_backu
 
 cp $SCRIPTDIR/index.md $SCRIPTDIR/backups/index_backup_$DATESTRING.md
 cp $SCRIPTDIR/stats.md $SCRIPTDIR/backups/stats_backup_$DATESTRING.md
+cp $SCRIPTDIR/activity.md $SCRIPTDIR/backups/activity_backup_$DATESTRING.md
