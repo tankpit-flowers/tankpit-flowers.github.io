0ad05a8791e2feb75c3770c011238ed89bbf9371
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-04-26 01:25:30 -0700

    Add files via upload

--- daily-backup.sh
+++ daily-backup.sh
@@ -4,11 +4,15 @@ SCRIPTDIR=/home/crosswalkcalvin/tankpit-flowers.github.io
 
 DATESTRING=$(date +%Y-%m-%d) # 2017-01-01
 
-cp $SCRIPTDIR/data/flowers_stats_daily.csv $SCRIPTDIR/data/flowers_stats_$DATESTRING.csv
+cp $SCRIPTDIR/data/flowers_stats_daily.csv $SCRIPTDIR/data/flowers_stats_archive/flowers_stats_$DATESTRING.csv
 cp $SCRIPTDIR/data/flowers_stats_now.csv $SCRIPTDIR/data/flowers_stats_daily.csv
 
-cp $SCRIPTDIR/data/flowers_hours_log.csv $SCRIPTDIR/data/flowers_hours_log_backup_$DATESTRING.csv
+cp $SCRIPTDIR/data/flowers_hours_log.csv $SCRIPTDIR/data/flowers_hours_log_backups/flowers_hours_log_$DATESTRING.csv
 
-cp $SCRIPTDIR/index.md $SCRIPTDIR/backups/index_backup_$DATESTRING.md
-cp $SCRIPTDIR/stats.md $SCRIPTDIR/backups/stats_backup_$DATESTRING.md
-cp $SCRIPTDIR/activity.md $SCRIPTDIR/backups/activity_backup_$DATESTRING.md
+cp $SCRIPTDIR/index.md $SCRIPTDIR/backups/index/index-$DATESTRING.md
+cp $SCRIPTDIR/stats.md $SCRIPTDIR/backups/stats/stats-$DATESTRING.md
+cp $SCRIPTDIR/stats-kills.md $SCRIPTDIR/backups/stats/stats-kills-$DATESTRING.md
+cp $SCRIPTDIR/stats-deact.md $SCRIPTDIR/backups/stats/stats-deact-$DATESTRING.md
+cp $SCRIPTDIR/activity.md $SCRIPTDIR/backups/activity/activity-$DATESTRING.md
+cp $SCRIPTDIR/activity-week.md $SCRIPTDIR/backups/activity/activity-week-$DATESTRING.md
+cp $SCRIPTDIR/activity-month.md $SCRIPTDIR/backups/activity/activity-month-$DATESTRING.md
