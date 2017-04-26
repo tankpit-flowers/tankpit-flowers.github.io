cf644e9a9b263a6b0bb9f43fb6064f70a9d22b1e
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-04-25 18:36:46 -0700

    Add files via upload

--- daily-backup.sh
+++ daily-backup.sh
@@ -6,3 +6,8 @@ DATESTRING=$(date +%Y-%m-%d) # 2017-01-01
 
 cp $SCRIPTDIR/data/flowers_stats_daily.csv $SCRIPTDIR/data/flowers_stats_$DATESTRING.csv
 cp $SCRIPTDIR/data/flowers_stats_now.csv $SCRIPTDIR/data/flowers_stats_daily.csv
+
+cp $SCRIPTDIR/data/flowers_hours_log.csv $SCRIPTDIR/data/flowers_hours_log_backup_$DATESTRING.csv
+
+cp $SCRIPTDIR/index.md $SCRIPTDIR/backups/index_backup_$DATESTRING.md
+cp $SCRIPTDIR/stats.md $SCRIPTDIR/backups/stats_backup_$DATESTRING.md
