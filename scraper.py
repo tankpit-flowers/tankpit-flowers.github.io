fc86b907f526dcd70a30ff39c7031b8eef5f0786
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-06-01 23:49:49 -0700

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -351,7 +351,7 @@ def get_diff_df(df, time_col = 'time'):
 
 def sum_diff(df, diff_col_name, time_col = 'time'):
     df = df.drop(time_col, axis = 1)
-    df = df[(df < 1) & (df > 0)]
+    df = df[(df < 20) & (df > 0)]
     df = change_nas_to_zeros(df)
     df = df.sum()
     df = df.reset_index(drop = False)
