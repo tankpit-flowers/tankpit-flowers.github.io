a66c2faf438297dff464ee80d9a1f600580f4497
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-06-11 21:32:28 -0700

    Update award-generator.js

--- scripts/award-generator.js
+++ scripts/award-generator.js
@@ -1,9 +1,5 @@
 function singleStar() {
 	$('#award-generator-0')
-    .addClass('awards-sprite a0-1')
-      .removeClass
-}
-
-function doubleStar() {
-	$('#award-generator-0').addClass('awards-sprite a0-1')
+    	.addClass('a0-1')
+     	.removeClass('a0-2 a0-3');
 }
