f1b35b06692367a0eeae504cf406c27aee929c3f
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-06-11 21:29:59 -0700

    Create award-generator.js

---
+++ scripts/award-generator.js
@@ -0,0 +1,9 @@
+function singleStar() {
+	$('#award-generator-0')
+    .addClass('awards-sprite a0-1')
+      .removeClass
+}
+
+function doubleStar() {
+	$('#award-generator-0').addClass('awards-sprite a0-1')
+}
