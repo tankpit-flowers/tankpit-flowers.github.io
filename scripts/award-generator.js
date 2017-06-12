b8b4f8afa7bc386672b45221e89f1c527202b7e2
tankpit-flowers <tankpit.flowers@yahoo.com>
2017-06-11 22:01:01 -0700

    Update award-generator.js

--- scripts/award-generator.js
+++ scripts/award-generator.js
@@ -33,3 +33,77 @@ function goldTank() {
         .addClass('a1-3')
         .removeClass('a1-1 a1-2');
 }
+
+function combatMedal() {
+    $('#award-generator-2')
+        .addClass('a2-1')
+        .removeClass('a2-2 a2-3');
+}
+
+function battleMedal() {
+    $('#award-generator-2')
+        .addClass('a2-2')
+        .removeClass('a2-1 a2-3');
+}
+
+function heroicMedal() {
+    $('#award-generator-2')
+        .addClass('a2-3')
+        .removeClass('a2-1 a2-2');
+}
+
+function shiningSword() {
+    $('#award-generator-3')
+        .addClass('a3-1')
+        .removeClass('a3-2 a3-3');
+}
+
+function batteredSword() {
+    $('#award-generator-3')
+        .addClass('a3-2')
+        .removeClass('a3-1 a3-3');
+}
+
+function rustySword() {
+    $('#award-generator-3')
+        .addClass('a3-3')
+        .removeClass('a3-1 a3-2');
+}
+
+function defenderOfTruth() {
+    $('#award-generator-4')
+        .addClass('a4-3');
+}
+
+function bronzeCup() {
+    $('#award-generator-5')
+        .addClass('a5-1')
+        .removeClass('a5-2 a5-3');
+}
+
+function silverCup() {
+    $('#award-generator-5')
+        .addClass('a5-2')
+        .removeClass('a5-1 a5-3');
+}
+
+function goldCup() {
+    $('#award-generator-5')
+        .addClass('a5-3')
+        .removeClass('a5-1 a5-2');
+}
+
+function purpleHeart() {
+    $('#award-generator-6')
+        .addClass('a6-1');
+}
+
+function warCorrespondent() {
+    $('#award-generator-7')
+        .addClass('a7-1');
+}
+
+function lightBulb() {
+    $('#award-generator-8')
+        .addClass('a8-1');
+}
